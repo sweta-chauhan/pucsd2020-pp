abcd051d324d432252048aa348a3ec8ae717766c
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-02-12 16:55:27 +0530

    all debug,warn,info,error,fatal level log function is added.

--- assignment-003/src/logger.c
+++ assignment-003/src/logger.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 
 l_error init(logger **log)
 {
@@ -31,4 +32,150 @@ void setup_logger(logger** log,char* filename,log_level level)
 	else
 		return;
 }
+
+const char* getlevel(log_level level)
+{
+	if(level==DEBUG)
+	{
+		return "DEBUG";
+	}
+	else
+	 {
+		if(level==WARN)
+		{
+			return "WARN";
+		}
+		else 
+			{
+				if(level==INFO)
+				{
+					return "INFO";
+				}
+				else 
+				{
+					if(level==ERROR)
+					{	
+					return "ERROR";
+					}
+					else if(level ==FATAL)
+					{
+					return "FATAL";
+					}
+			}
+		}
+	}
+}
+
+void logger_(logger *log,char *message,log_level level)
+{
+	if (log->level<= level)
+	{
+		switch(level)
+		{
+			case DEBUG :
+						debug(log,message,level);
+						break;
+			case WARN :
+					warn(log,message,level);
+					break;
+			case INFO :
+					info(log,message,level);
+					break;
+			case ERROR :
+					error(log,message,level);
+					break;
+			case  FATAL :
+					fatal(log,message,level);
+					  break;
+			case DEFAULT :
+					  break;
+		}
+	}
+	return;
+}
+
+void debug(logger *log,char* message,log_level level)
+{
+	if(log->level<=level)
+	{
+
+	time_t start;
+	struct tm* pt;
+	start = time(NULL);
+	pt = localtime(&start);
+	char *time = asctime(pt);
+	if (time[strlen(time)-1] == '\n') 
+		time[strlen(time)-1] = '\0';
+	fprintf(log->fp,"%s :%s :%s :%s\n",log->filename,getlevel(log->level),time,message);
+	}
+	return;
+}
+
+void warn(logger* log,char* message,log_level level)
+{
+	if(log->level<=level)
+	{
+
+	time_t start;
+	struct tm* pt;
+	start = time(NULL);
+	pt = localtime(&start);
+	char *time = asctime(pt);
+	if (time[strlen(time)-1] == '\n') 
+		time[strlen(time)-1] = '\0';
+	fprintf(log->fp,"%s :%s :%s :%s\n",log->filename,getlevel(log->level),time,message);
+	}
+	return;
+}
+
+void info(logger *log,char* message,log_level level)
+{
+	if(log->level<=level)
+	{
+
+	time_t start;
+	struct tm* pt;
+	start = time(NULL);
+	pt = localtime(&start);
+	char *time = asctime(pt);
+	if (time[strlen(time)-1] == '\n') 
+		time[strlen(time)-1] = '\0';
+	fprintf(log->fp,"%s :%s :%s :%s\n",log->filename,getlevel(level),time,message);
+	}
+	return;
+}
+
+void error(logger *log,char*message,log_level level)
+{
+	if(log->level<=level)
+	{
+	time_t start;
+	struct tm* pt;
+	start = time(NULL);
+	pt = localtime(&start);
+	char *time = asctime(pt);
+	if (time[strlen(time)-1] == '\n') 
+		time[strlen(time)-1] = '\0';
+	fprintf(log->fp,"%s :%s :%s :%s\n",log->filename,getlevel(level),time,message);
+	}
+	return;
+}
+
+void fatal(logger *log,char *message,log_level level)
+{
+	if(log->level<=level)
+	{
+
+	time_t start;
+	struct tm* pt;
+	start = time(NULL);
+	pt = localtime(&start);
+	char *time = asctime(pt);
+	if (time[strlen(time)-1] == '\n') 
+		time[strlen(time)-1] = '\0';
+	fprintf(log->fp,"%s :%s :%s :%s\n",log->filename,getlevel(level),time,message);
+	}
+	return;
+}
+
 
