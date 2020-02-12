d6c117c34de1da9f0c4890991ce5e3050de7e165
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-02-12 18:59:27 +0530

    function testing

--- assignment-003/tests/function/log_func.c
+++ assignment-003/tests/function/log_func.c
@@ -4,21 +4,23 @@
 
 int main(void)
 {
-	logger *log_;
+	logger *log_[5];
 	char filename[][15]={"debug.log","warn.log","info.log","error.log","fatal.log"};
 	log_level level[6]={DEBUG,WARN,INFO,ERROR,FATAL};
 	for (int i = 0 ; i<5 ; i++)
 	{
-		setup_logger(&log_,filename[i],level[i]);
-		if(log_!=NULL)
+		setup_logger(&log_[i],filename[i],level[i]);
+		
+		if(log_[i]!=NULL)
 		{
-			logger_(log_,"Started",DEBUG);
-			logger_(log_,"Info check",INFO);
-			logger_(log_,"Warning check",WARN);
-			logger_(log_,"Error check",ERROR);
-			logger_(log_,"Fatal check",FATAL);
-			fclose(log_->fp);
-			remove(log_->filename);
+			logger_(log_[i],"Started",DEBUG);
+			logger_(log_[i],"Info check",INFO);
+			logger_(log_[i],"Warning check",WARN);
+			logger_(log_[i],"Error check",ERROR);
+			logger_(log_[i],"Fatal check",FATAL);
+			fclose(log_[i]->fp);
+			free_logger();
+			remove(log_[i]->filename);
 		}
 		else
 			return -1;
