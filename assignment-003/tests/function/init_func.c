8bee968f764042dc8e3c8e3321a87490a55728c0
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-02-14 02:52:16 +0530

    init function testing.

--- assignment-003/tests/function/init_func.c
+++ assignment-003/tests/function/init_func.c
@@ -1,12 +1,26 @@
+
+
+
 #include"../../include/logger.h"
 #include<stdio.h>
 
 int main(void)
 {
-	logger *log;
-	if(init(&log))
+	logger *log,*log1;
+	if(init(&log)==SUCCESS)
 	{
+		printf("One logger object is created ...\n");
+	}
+	if(init(&log1)==SUCCESS)
+	{
+		printf("Second logger object created ...\n");
 		return 0;
 	}
+
+	else
+		{
+			printf("Second logger object couldn't created due to Singleton Pattern\n");
+		}
+	
 	return -1;
 }
