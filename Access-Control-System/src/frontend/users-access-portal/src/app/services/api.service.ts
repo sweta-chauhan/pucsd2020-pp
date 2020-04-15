f094f4dab09e6ab0537b4adb334ac5b142602b0e
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-04-16 02:39:50 +0530

    All backend api services call are written here.

--- Access-Control-System/src/frontend/users-access-portal/src/app/services/api.service.ts
+++ Access-Control-System/src/frontend/users-access-portal/src/app/services/api.service.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Observable  } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
+
+@Injectable({  
+	  providedIn: 'root'  
 })
 export class ApiService {
 
-  constructor(private httpclient: HttpClient) { }
+  private BASE_URL = "http://localhost:9090/webapi/v1/user"
 
-  getuserbyid(): Observable<any> {
-  	return this.httpclient.get("http://localhost:9090/webapi/v1/user/1")
+  constructor(private httpclient: HttpClient) { }
+  
+  /*Create Object*/
+  createObject(body:any|null):Observable<any>{
+   return this.httpclient.post(this.BASE_URL,body);
+    }
+
+  /*Get Object by id*/
+  getById(id: number):Observable<any>{
+	 return this.httpclient.get(this.BASE_URL+"/"+id);
+	  }
+  
+  /*Get All object */
+  getAll(): Observable<any> {
+  	return this.httpclient.get(this.BASE_URL);
   }
+ 
+ /*Delete User by id*/
+ DelById(id: number,body:any|null):Observable<any>{
+   return this.httpclient.post(this.BASE_URL+"/"+id,body);
+    }
+ /*Update User by id*/
+  UpdateById(id: number,body: any|null):Observable<any>{
+   return this.httpclient.put(this.BASE_URL+"/"+id,body);
+    }
 }
