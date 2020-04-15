20d50de9771c5fc41d37d927ba53addbbdba387b
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-04-16 02:41:13 +0530

    Some App level changes to perform operations.

--- Access-Control-System/src/frontend/users-access-portal/src/app/app-routing.module.ts
+++ Access-Control-System/src/frontend/users-access-portal/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { AddComponent } from './components/User/add/add.component';
+import { DeleteComponent } from './components/User/delete/delete.component';
+import { UpdateComponent } from './components/User/update/update.component';
+import { SearchComponent } from './components/User/search/search.component';
+import { DisplayComponent } from './components/User/display/display.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+{path: 'home',component:HomeComponent},
+{path: 'add/user',component:AddComponent},
+{path: 'edit/user/:id',component:UpdateComponent},
+{path: 'delete/user/:id',component:DeleteComponent },
+{path: 'search/user/:id',component:SearchComponent},
+{path: 'display/user',component:DisplayComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Access-Control-System/src/frontend/users-access-portal/src/app/app.module.ts
+++ Access-Control-System/src/frontend/users-access-portal/src/app/app.module.ts
@@ -1,18 +1,71 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+
+import { ApiService } from './services/api.service';
+import { NavigatorComponent } from './components/navigator/navigator.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+/*for Form*/
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { AddComponent } from './components/User/add/add.component';
+import { DeleteComponent } from './components/User/delete/delete.component';
+import { UpdateComponent } from './components/User/update/update.component';
+import { SearchComponent } from './components/User/search/search.component'
+import { HomeComponent } from './components/home/home.component';
+import { DisplayComponent } from './components/User/display/display.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavigatorComponent,
+    AddComponent,
+    DeleteComponent,
+    UpdateComponent,
+    SearchComponent,
+    DisplayComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+	AppRoutingModule,
+	RouterModule,
+    HttpClientModule,
+	AppRoutingModule,
+	LayoutModule,
+	MatToolbarModule,
+	MatButtonModule,
+	MatSidenavModule,
+	MatIconModule,
+	MatListModule,
+	BrowserAnimationsModule,
+	MatGridListModule,
+	MatCardModule,
+	MatMenuModule,
+  MatFormFieldModule,
+  FormsModule, 
+  ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+  ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
