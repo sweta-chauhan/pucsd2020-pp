fa360951dd08039df0439d95004147e107fe4a1c
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-05-15 12:47:52 +0530

    Resource model updation.

--- Access-Control-System/src/backend/rest-api/model/resource.go
+++ Access-Control-System/src/backend/rest-api/model/resource.go
@@ -2,11 +2,11 @@ package model
 
 type Resource struct {
     Id            int64   `json:"id,omitempty" key:"primary" autoincr:"1" column:"id"`
-    ResourceType  int64   `json:"resource_id" column:"resource_type_id"`
-    Parent        int64   `json:"parent_id" column:"parent_resource_id"`
+    ResourceType  int64   `json:"rtype" column:"resource_type_id"`
+    Parent        int64   `json:"prnt_id" column:"parent_resource_id"`
 	ResourceName  string  `json:"rname" column:"resource_name"`
-	ResourcePath  string  `json:"resource_id" column:"location"`
-	Owner		  int64   `json:"resource_id" column:"owner"`
+	ResourcePath  string  `json:"rpath" column:"location"`
+	Owner		  int64   `json:"owner" column:"owner"`
 }
 
 func (resource *Resource) Table() string {
