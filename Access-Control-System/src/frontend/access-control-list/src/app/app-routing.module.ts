3e4695fdd31149144053bf001dedd0729ae4c564
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-05-17 04:13:22 +0530

    Routing with guard

--- Access-Control-System/src/frontend/access-control-list/src/app/app-routing.module.ts
+++ Access-Control-System/src/frontend/access-control-list/src/app/app-routing.module.ts
@@ -4,13 +4,20 @@ import {UserportalComponent} from './component/userportal/userportal.component';
 import { LoginComponent } from './component/login/login.component';
 import { AboutComponent } from './component/about/about.component';
 import { MainComponent } from './component/main/main.component';
+import { RegisterUserComponent } from './component/register-user/register-user.component';
+import { PermissionResourseComponent } from './component/permission-resourse/permission-resourse.component';
+import { GroupComponent } from './component/group/group.component';
+import { MemberGroupComponent } from './component/member-group/member-group.component';
+import { ResourceComponent } from './component/resource/resource.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'main', component: MainComponent },
-  { path: 'userportal', component: UserportalComponent },
-  { path: 'login', component: LoginComponent },
-  { path : 'about', component : AboutComponent},
-
+  { path: 'main', component: MainComponent, canActivate: [AuthGuard]},
+  { path: 'userportal', component: UserportalComponent,canActivate: [AuthGuard] },
+  { path: '', component: LoginComponent },
+  { path: 'resource',  component: ResourceComponent,canActivate: [AuthGuard]},
+  { path : 'group', component : GroupComponent,canActivate: [AuthGuard]},
+  { path : 'user', component : RegisterUserComponent,canActivate: [AuthGuard]},
 ];
 @NgModule({
   imports: [
