132c53edfaa1ea4342a5b51d6d653871380853f0
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-05-17 04:12:34 +0530

    Component Value of app

--- Access-Control-System/src/frontend/access-control-list/src/app/app.component.ts
+++ Access-Control-System/src/frontend/access-control-list/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { environment } from './../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Resourcetype } from './models/resourcetype';
-
-import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +9,10 @@ import { ApiService } from './services/api.service';
 })
 export class AppComponent {
   title = 'access-control-list';
-  constructor(private _api: ApiService) { }
-  group;
+  constructor() { }
   ngOnInit(): void {
+	}	
 
-	}
-    
+
+     
 }
