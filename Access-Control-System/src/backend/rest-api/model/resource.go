d3b03792f50c67dac16c85de71d4a442955ad459
Sweta Kumari <swetachouhan12101995@gmail.com>
2020-05-22 18:13:43 +0530

    All backend models are defined here.

--- Access-Control-System/src/backend/rest-api/model/resource.go
+++ Access-Control-System/src/backend/rest-api/model/resource.go
@@ -7,7 +7,7 @@ type Resource struct {
 	ResourceName  string  `json:"rname" column:"resource_name"`
 	ResourcePath  string  `json:"rpath" column:"location"`
 	Owner		  int64   `json:"owner" column:"owner"`
-	Data		  string  `json:"datefield" column:"creation_date"`
+	Data		  string  `json:"data,omitempty"`
 }
 
 func (resource *Resource) Table() string {
